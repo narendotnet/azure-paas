8cf9009c38bb2b6da27ff1ca719cf068e3ddec3c
Narendrakumar Sethuraman <snaren0423@gmai.com>
2022-09-19 13:04:37 +0930

    updated models

--- BigPurpleBankWebApi/Models/Data.cs
+++ BigPurpleBankWebApi/Models/Data.cs
@@ -8,21 +8,223 @@ namespace BigPurpleBankWebApi.Models
         {
             new Product()
             {
-                ProductId= "123",
+                ProductId= "A000",
+                EffectiveFrom="2012-12-25T15:43:00-08:00",
+                EffectiveTo="2021-12-25T15:43:00-08:00",
+                LastUpdated="2012-12-25T15:43:00-08:00",
+                ProductCategory=ProductCategory.TERM_DEPOSITS,
+                Name= "Trust Loan",
+                Description= "This is a new termdeposits loan product",
+                Brand="test",
+                BrandName="test",
+                ApplicationUri="test",
+                IsTailored=true,
+                AdditionalInformation= new AdditionalInformation()
+                {
+                    OverviewUri="test",
+                    TermsUri="test",
+                    EligibilityUri="test",
+                    FeesAndPricingUri="test",
+                    BundleUri="test"
+                }
+            },
+            new Product()
+            {
+                ProductId= "A100",
+                EffectiveFrom="2012-12-25T15:43:00-08:00",
+                EffectiveTo="2021-12-25T15:43:00-08:00",
+                LastUpdated="2012-12-25T15:43:00-08:00",
+                ProductCategory=ProductCategory.PERS_LOANS,
                 Name= "Personal Loan",
-                Description= "this is a new product"
+                Description= "This is a new personal loan product",
+                Brand="test",
+                BrandName="test",
+                ApplicationUri="test",
+                IsTailored=true,
+                AdditionalInformation= new AdditionalInformation()
+                {
+                    OverviewUri="test",
+                    TermsUri="test",
+                    EligibilityUri="test",
+                    FeesAndPricingUri="test",
+                    BundleUri="test"
+                }
             },
             new Product()
             {
-                ProductId= "456",
-                Name= "Car Loan",
-                Description= "this is a new product"
+                ProductId= "A200",
+                EffectiveFrom="2012-12-25T15:43:00-08:00",
+                EffectiveTo="2021-12-25T15:43:00-08:00",
+                LastUpdated="2012-12-25T15:43:00-08:00",
+                ProductCategory=ProductCategory.RESIDENTIAL_MORTGAGES,
+                Name= "House Loan",
+                Description= "This is a new house loan product",
+                Brand="test",
+                BrandName="test",
+                ApplicationUri="test",
+                IsTailored=true,
+                AdditionalInformation= new AdditionalInformation()
+                {
+                    OverviewUri="test",
+                    TermsUri="test",
+                    EligibilityUri="test",
+                    FeesAndPricingUri="test",
+                    BundleUri="test"
+                }
             },
             new Product()
             {
-                ProductId= "789",
-                Name= "Houses Loan",
-                Description= "this is a new product"
+                ProductId= "A300",
+                EffectiveFrom="2012-12-25T15:43:00-08:00",
+                EffectiveTo="2021-12-25T15:43:00-08:00",
+                LastUpdated="2012-12-25T15:43:00-08:00",
+                ProductCategory=ProductCategory.LEASES,
+                Name= "Lease Loan",
+                Description= "This is a new lease loan product",
+                Brand="test",
+                BrandName="test",
+                ApplicationUri="test",
+                IsTailored=true,
+                AdditionalInformation= new AdditionalInformation()
+                {
+                    OverviewUri="test",
+                    TermsUri="test",
+                    EligibilityUri="test",
+                    FeesAndPricingUri="test",
+                    BundleUri="test"
+                }
+            },
+            new Product()
+            {
+                ProductId= "A400",
+                EffectiveFrom="2012-12-25T15:43:00-08:00",
+                EffectiveTo="2021-12-25T15:43:00-08:00",
+                LastUpdated="2012-12-25T15:43:00-08:00",
+                ProductCategory=ProductCategory.TRADE_FINANCE,
+                Name= "Trade Loan",
+                Description= "This is a new trade loan product",
+                Brand="test",
+                BrandName="test",
+                ApplicationUri="test",
+                IsTailored=true,
+                AdditionalInformation= new AdditionalInformation()
+                {
+                    OverviewUri="test",
+                    TermsUri="test",
+                    EligibilityUri="test",
+                    FeesAndPricingUri="test",
+                    BundleUri="test"
+                }
+            },
+            new Product()
+            {
+                ProductId= "A500",
+                EffectiveFrom="2012-12-25T15:43:00-08:00",
+                EffectiveTo="2021-12-25T15:43:00-08:00",
+                LastUpdated="2012-12-25T15:43:00-08:00",
+                ProductCategory=ProductCategory.REGULATED_TRUST_ACCOUNTS,
+                Name= "Trust Loan",
+                Description= "This is a new trust loan product",
+                Brand="test",
+                BrandName="test",
+                ApplicationUri="test",
+                IsTailored=true,
+                AdditionalInformation= new AdditionalInformation()
+                {
+                    OverviewUri="test",
+                    TermsUri="test",
+                    EligibilityUri="test",
+                    FeesAndPricingUri="test",
+                    BundleUri="test"
+                }
+            },
+            new Product()
+            {
+                ProductId= "A600",
+                EffectiveFrom="2012-12-25T15:43:00-08:00",
+                EffectiveTo="2021-12-25T15:43:00-08:00",
+                LastUpdated="2012-12-25T15:43:00-08:00",
+                ProductCategory=ProductCategory.BUSINESS_LOANS,
+                Name= "Trust Loan",
+                Description= "This is a new business loan product",
+                Brand="test",
+                BrandName="test",
+                ApplicationUri="test",
+                IsTailored=true,
+                AdditionalInformation= new AdditionalInformation()
+                {
+                    OverviewUri="test",
+                    TermsUri="test",
+                    EligibilityUri="test",
+                    FeesAndPricingUri="test",
+                    BundleUri="test"
+                }
+            },
+            new Product()
+            {
+                ProductId= "A700",
+                EffectiveFrom="2012-12-25T15:43:00-08:00",
+                EffectiveTo="2021-12-25T15:43:00-08:00",
+                LastUpdated="2012-12-25T15:43:00-08:00",
+                ProductCategory=ProductCategory.CRED_AND_CHRG_CARDS,
+                Name= "Trust Loan",
+                Description= "This is a new cards loan product",
+                Brand="test",
+                BrandName="test",
+                ApplicationUri="test",
+                IsTailored=true,
+                AdditionalInformation= new AdditionalInformation()
+                {
+                    OverviewUri="test",
+                    TermsUri="test",
+                    EligibilityUri="test",
+                    FeesAndPricingUri="test",
+                    BundleUri="test"
+                }
+            },
+            new Product()
+            {
+                ProductId= "A800",
+                EffectiveFrom="2012-12-25T15:43:00-08:00",
+                EffectiveTo="2021-12-25T15:43:00-08:00",
+                LastUpdated="2012-12-25T15:43:00-08:00",
+                ProductCategory=ProductCategory.CRED_AND_CHRG_CARDS,
+                Name= "Trust Loan",
+                Description= "This is a new cards loan product",
+                Brand="test",
+                BrandName="test",
+                ApplicationUri="test",
+                IsTailored=true,
+                AdditionalInformation= new AdditionalInformation()
+                {
+                    OverviewUri="test",
+                    TermsUri="test",
+                    EligibilityUri="test",
+                    FeesAndPricingUri="test",
+                    BundleUri="test"
+                }
+            },
+            new Product()
+            {
+                ProductId= "A900",
+                EffectiveFrom="2012-12-25T15:43:00-08:00",
+                EffectiveTo="2021-12-25T15:43:00-08:00",
+                LastUpdated="2012-12-25T15:43:00-08:00",
+                ProductCategory=ProductCategory.OVERDRAFTS,
+                Name= "Trust Loan",
+                Description= "This is a new overdrafts loan product",
+                Brand="test",
+                BrandName="test",
+                ApplicationUri="test",
+                IsTailored=true,
+                AdditionalInformation= new AdditionalInformation()
+                {
+                    OverviewUri="test",
+                    TermsUri="test",
+                    EligibilityUri="test",
+                    FeesAndPricingUri="test",
+                    BundleUri="test"
+                }
             }
         };
     }
--- BigPurpleBankWebApi/Models/Product.cs
+++ BigPurpleBankWebApi/Models/Product.cs
@@ -1,10 +1,96 @@
-﻿namespace BigPurpleBankWebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BigPurpleBankWebApi.Models
 {
     public class Product
     {
+        [Required]
         public string ProductId { get; set; }
+        public string EffectiveFrom { get; set; }
+        public string EffectiveTo { get; set; }
+        [Required]
+        public string LastUpdated { get; set; }
+        [Required]
+        public ProductCategory ProductCategory { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        public string BrandName { get; set; }
+        public string ApplicationUri { get; set; }
+        [Required]
+        public bool IsTailored { get; set; }
+        public AdditionalInformation AdditionalInformation { get; set; }
+        public List<CardArt> CardArt { get; set; }
 
+    }
+
+    public class CardArt
+    {
+        public string Title { get; set; }
+        public string ImageUri { get; set; }
+    }
+
+    public class AdditionalInformation
+    {
+        public string OverviewUri { get; set; }
+        public string TermsUri { get; set; }
+        public string EligibilityUri { get; set; }
+        public string FeesAndPricingUri { get; set; }
+        public string BundleUri { get; set; }
+        public List<AdditionalOverviewUris> AdditionalOverviewUris { get; set; }
+        public List<AdditionalTermUris> AdditionalTermUris { get; set; }
+        public List<AdditionalEligibilityUris> AdditionalEligibilityUris { get; set; }
+        public List<AdditionalFeesAndPricingUris> AdditionalFeesAndPricingUris { get; set; }
+        public List<AdditionalBundleUris> AdditionalBundleUris { get; set; }
+
+    }
+
+    public class AdditionalBundleUris
+    {
+        public string Description { get; set; }
+        public string AdditionalInfoUri { get; set; }
+    }
+
+    public class AdditionalFeesAndPricingUris
+    {
+        public string Description { get; set; }
+        public string AdditionalInfoUri { get; set; }
+    }
+
+    public class AdditionalEligibilityUris
+    {
+        public string Description { get; set; }
+        public string AdditionalInfoUri { get; set; }
+    }
+
+    public class AdditionalTermUris
+    {
+        public string Description { get; set; }
+        public string AdditionalInfoUri { get; set; }
+    }
+
+    public class AdditionalOverviewUris
+    {
+        public string Description { get; set; }
+        public string AdditionalInfoUri { get; set; }
+    }
+
+    public enum ProductCategory
+    {
+        BUSINESS_LOANS,
+        CRED_AND_CHRG_CARDS,
+        LEASES,
+        MARGIN_LOANS,
+        OVERDRAFTS,
+        PERS_LOANS,
+        REGULATED_TRUST_ACCOUNTS,
+        RESIDENTIAL_MORTGAGES,
+        TERM_DEPOSITS,
+        TRADE_FINANCE,
+        TRANS_AND_SAVINGS_ACCOUNTS,
+        TRAVEL_CARDS
     }
 }
